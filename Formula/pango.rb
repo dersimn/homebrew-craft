3663749b79bbe564b4bc361b3145fe9df4c8bbcf
Simon Christmann <simon@christmann.email>
2020-08-10 13:45:40 +0200

    Add Pango 1.45.5
    Official homebrew-core uses 1.44.7, new version fixes https://stackoverflow.com/questions/57698204/gnuplot-pdf-terminal-exhibits-font-issues-on-mac

---
+++ Formula/pango.rb
@@ -0,0 +1,82 @@
+class Pango < Formula
+  desc "Framework for layout and rendering of i18n text"
+  homepage "https://www.pango.org/"
+  url "https://download.gnome.org/sources/pango/1.45/pango-1.45.5.tar.xz"
+  sha256 "f61dd911de2d3318b43bbc56bd271637a46f9118a1ee4378928c06df8a1c1705"
+
+  head do
+    url "https://gitlab.gnome.org/GNOME/pango.git"
+  end
+
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on "fribidi"
+  depends_on "glib"
+  depends_on "harfbuzz"
+
+  def install
+    mkdir "build" do
+      system "meson", *std_meson_args,
+                      "-Ddefault_library=both",
+                      "-Dintrospection=true",
+                      "-Duse_fontconfig=true",
+                      ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
+  end
+
+  test do
+    system "#{bin}/pango-view", "--version"
+    (testpath/"test.c").write <<~EOS
+      #include <pango/pangocairo.h>
+
+      int main(int argc, char *argv[]) {
+        PangoFontMap *fontmap;
+        int n_families;
+        PangoFontFamily **families;
+        fontmap = pango_cairo_font_map_get_default();
+        pango_font_map_list_families (fontmap, &families, &n_families);
+        g_free(families);
+        return 0;
+      }
+    EOS
+    cairo = Formula["cairo"]
+    fontconfig = Formula["fontconfig"]
+    freetype = Formula["freetype"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    harfbuzz = Formula["harfbuzz"]
+    libpng = Formula["libpng"]
+    pixman = Formula["pixman"]
+    flags = %W[
+      -I#{cairo.opt_include}/cairo
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{harfbuzz.opt_include}/harfbuzz
+      -I#{include}/pango-1.0
+      -I#{libpng.opt_include}/libpng16
+      -I#{pixman.opt_include}/pixman-1
+      -D_REENTRANT
+      -L#{cairo.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -lcairo
+      -lglib-2.0
+      -lgobject-2.0
+      -lintl
+      -lpango-1.0
+      -lpangocairo-1.0
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
